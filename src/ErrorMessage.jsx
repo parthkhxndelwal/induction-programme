6bcf8893f9a889899c7fd3795bbe292a25e6123e
Parth Khandelwal <parthkhandelwal.dev@gmail.com>
2024-08-26 09:10:06 +0530

    Update

--- src/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorMessage = ({ message }) => (
-  <div className="text-danger m-2 w-30 text-center" role="alert">
-    {message}
-  </div>
-);
-
-export default ErrorMessage;
